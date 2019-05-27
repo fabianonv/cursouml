c76a8c9490ea420e0c4fa007dfb463e9b8abc2e9
Fabiano <fabianonv@hotmail.com>
2019-05-27 00:36:57 -0300

    Criando operacao de instanciacao(mais erros)

--- src/main/java/com/cursouml/CursoumlApplication.java
+++ src/main/java/com/cursouml/CursoumlApplication.java
@@ -1,13 +1,32 @@
 package com.cursouml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursouml.domain.Categoria;
+import com.cursouml.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoumlApplication {
+public class CursoumlApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoumlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
