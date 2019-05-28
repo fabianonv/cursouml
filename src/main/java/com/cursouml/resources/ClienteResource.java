c2bcdab53b138a6c275dc958af26436be99c5896
Fabiano <fabianonv@hotmail.com>
2019-05-28 00:33:27 -0300

    Endpoint /clientes/{id} disponivel (e erros)

---
+++ src/main/java/com/cursouml/resources/ClienteResource.java
@@ -0,0 +1,37 @@
+package com.cursouml.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursouml.domain.Cliente;
+import com.cursouml.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value="/(id)", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Cliente obj = service.buscar(id);
+		
+		/*Cliente cat1 = new Cliente(1, "Informática");
+		Cliente cat2 = new Cliente(2, "Escritório");		
+		List<Cliente> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);		
+		return lista;*/
+		
+		return ResponseEntity.ok().body(obj);
+	}
+}
+	
