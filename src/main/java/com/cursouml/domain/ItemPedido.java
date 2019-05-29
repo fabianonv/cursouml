ed0c8eec9b6eb1f1912ea3e0ec0755ccb5c055d2
Fabiano <fabianonv@hotmail.com>
2019-05-28 23:53:52 -0300

    Endpoint / pedidos/{id} disponibilizado (n roda)

--- src/main/java/com/cursouml/domain/ItemPedido.java
+++ src/main/java/com/cursouml/domain/ItemPedido.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -13,6 +15,7 @@ public class ItemPedido implements Serializable {
 	private Integer quantidade;
 	private Double preco; 
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -30,9 +33,11 @@ public class ItemPedido implements Serializable {
 		
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+	
 	
 	public Produto getProduto() {
 		return id.getProduto();
